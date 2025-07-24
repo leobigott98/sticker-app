8a94710cdaa98d48b3b0ec596de2b62f3de7930a
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-23 20:55:08 -0400

    added fetch images visual

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -7,9 +7,11 @@ const sql = postgres(process.env.SUPABASE_POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchStickerImages(): Promise<StickerImageType[] | null> {
     try {
+        console.log('Fetching sticker images...');
         const response = await sql<StickerImageType[]>`
             SELECT * FROM stickers
         `;
+        console.log('Fetched:', response.length, 'stickers');
         return response 
         
     } catch (error) {
